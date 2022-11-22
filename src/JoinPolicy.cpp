9c275ff02da4e1f05e223aef4ebc5a15bf1c03c4
Bershco <roee.hersco@gmail.com>
2022-11-22 22:28:32 +0200

    Added abs()

--- src/JoinPolicy.cpp
+++ src/JoinPolicy.cpp
@@ -5,7 +5,7 @@ const Coalition& MandatesJoinPolicy::join(vector<Coalition> offerers) const
 {
 	Coalition _c = offerers[0];
 	int maxMandates = _c.getMandates();
-	for (int i = 0; i < offerers.size(); i++)
+	for (int i = 0; i < abs(offerers.size()); i++)
 		if (offerers[i].getMandates() > maxMandates)
 			_c = offerers[i];
 	return *(new Coalition(_c)); //resolved
