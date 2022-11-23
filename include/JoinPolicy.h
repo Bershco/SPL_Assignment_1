5d39cecda4ac2109604be359e3cff0e2c654ada6
Bershco <roee.hersco@gmail.com>
2022-11-23 22:17:25 +0200

    Added parameter

--- include/JoinPolicy.h
+++ include/JoinPolicy.h
@@ -10,20 +10,20 @@ class Agent;
 
 class JoinPolicy {
 public:
-	virtual Agent* join(vector<Coalition*> offerers, Party& p) = 0;
+	virtual Agent* join(vector<Coalition*> offerers, Party& p, int aId) = 0;
 	virtual JoinPolicy* clone();
 	virtual ~JoinPolicy() = default;
 };
 
 class MandatesJoinPolicy : public JoinPolicy {
 public:
-	Agent* join(vector<Coalition*> offerers, Party& p);
+	Agent* join(vector<Coalition*> offerers, Party& p, int aId);
 	JoinPolicy* clone();
 };
 
 class LastOfferJoinPolicy : public JoinPolicy {
 public:
-	Agent* join(vector<Coalition*> offerers, Party& p);
+	Agent* join(vector<Coalition*> offerers, Party& p, int aId);
 	JoinPolicy* clone();
 };
 
